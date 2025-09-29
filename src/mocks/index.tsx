a615397770c5cdfe39a8109b86e61aaa9d97268c
raoul <raouldhofmann@gmail.com>
2025-09-29 20:57:23 +0800

    allow mocking when static page

--- src/mocks/index.tsx
+++ src/mocks/index.tsx
@@ -1,7 +1,9 @@
 import { worker } from './browers'
 
+const isGitHubPages = process.env.GH_PAGES === 'true'
+
 export async function enableMocking() {
-  if (import.meta.env.DEV || import.meta.env.GH_PAGES === 'true') {
+  if (import.meta.env.DEV || isGitHubPages) {
     return worker.start({
       serviceWorker: {
         url: import.meta.env.DEV ? '/mockServiceWorker.js' : '/react-demo/mockServiceWorker.js'
