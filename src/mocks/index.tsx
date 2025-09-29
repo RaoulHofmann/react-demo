7231e941a80ff3f324399b18183867cdf7590696
raoul <raouldhofmann@gmail.com>
2025-09-29 20:56:43 +0800

    allow mocking when static page

--- src/mocks/index.tsx
+++ src/mocks/index.tsx
@@ -2,6 +2,11 @@ import { worker } from './browers'
 
 export async function enableMocking() {
   if (import.meta.env.DEV || import.meta.env.GH_PAGES === 'true') {
-    return worker.start({ onUnhandledRequest: 'bypass' })
+    return worker.start({
+      serviceWorker: {
+        url: import.meta.env.DEV ? '/mockServiceWorker.js' : '/react-demo/mockServiceWorker.js'
+      },
+      onUnhandledRequest: 'bypass'
+    })
   }
 }
