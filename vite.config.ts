253f35ec17d43f205bc97eba59f20ee031df4889
raoul <raouldhofmann@gmail.com>
2025-09-29 20:49:55 +0800

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.GH_PAGES === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     global: 'globalThis',
-  }
+  },
+  base: isGitHubPages ? '/react-demo/' : '/',
 })
